be789876e98288e4d8a944608591578f0ce460a7
Ayodeji Adebayo <aadebay2@users.noreply.github.com>
2019-08-08 16:34:03 -0500

    mapped API response
    Mapped the API hits to a reusable component (recipie.js). the component is rendered multiple times based on the number of hits

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-
+import Recipes from "./recipe";
 import "./styles.css";
 
-function App() {
+const App = () => {
+  const App_ID = process.env.REACT_APP_ID;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  //const App_ID = "44f266c3";
+  //const API_KEY = "1bffac3b00abb31de49bb37e1928f153";
+
+  const APIRequest = `https://api.edamam.com/search?q=chicken&app_id=${App_ID}&app_key=${API_KEY}`;
+
+  const [recipes, setRecipesArray] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  //make async calls to the api
+  var getRecipes = async () => {
+    const response = await fetch(APIRequest);
+    const data = await response.json();
+    setRecipesArray(data.hits);
+    console.log(data.hits);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1> Hello React</h1>
+      <form className="search_form">
+        <input tyoe="text" className="search_bar" />
+        <button type="submit" className="search_button">
+          search
+        </button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipes />
+      ))}
     </div>
   );
-}
+};
 
+//export default App;
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
---
+++ src/recipe.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Recipes = () => {
+  return (
+    //this component would be used to render all results from the API query
+    <div>
+      <h1> Title </h1>
+      <p> Calories </p>
+      <image src="" alt="" />
+    </div>
+  );
+};
+
+export default Recipes;
