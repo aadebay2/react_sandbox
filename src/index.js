81a0a8dd416eb668b055db9f9ef12d13764ff415
Ayodeji Adebayo <aadebay2@users.noreply.github.com>
2019-08-08 14:25:42 -0500

    initial commit
    A starting checkpoint for the APP

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
 
-function App() {
+const App = () => {
+  const App_ID = process.env.REACT_APP_ID;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const ExampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${App_ID}&app_key=${API_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1> Hello React</h1>
+      <form className="search_form">
+        <input tyoe="text" className="search_bar" />
+        <button type="submit" className="search_button">
+          search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
+//export default App;
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
